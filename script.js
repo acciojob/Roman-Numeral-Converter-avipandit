e264f12f4b5a0205c641b6a9c9b58c468bedd419
AVINASH KUMAR <65335071+avipandit@users.noreply.github.com>
2023-01-13 20:41:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
-function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+function integer_to_roman(num) {
+if (typeof num !== 'number') 
+return false; 
 
-  var str = '';
-
-  for (var i of Object.keys(roman)) {
-    var q = Math.floor(num / roman[i]);
-    num -= q * roman[i];
-    str += i.repeat(q);
-  }
-
-  return str;
-
+var digits = String(+num).split(""),
+key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+"","I","II","III","IV","V","VI","VII","VIII","IX"],
+roman_num = "",
+i = 3;
+while (i--)
+roman_num = (key[+digits.pop() + (i * 10)] || "") + roman_num;
+return Array(+digits.join("") + 1).join("M") + roman_num;
 }
+  
+
+
+
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
+console.log(integer_to_roman(36));
 
 
 
